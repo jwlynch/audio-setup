87a919c20463547c97ae86961bbf4abc44832445
Jim <jim@jam.sessionsnet.org>
2022-04-12 00:00:09 -0700

    script to help find sound output device names

---
+++ find-out.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in $(aplay -L | grep "^[^\ ]")
+do
+    echo test device is $i
+    speaker-test -t wav -c 2 -l 1 -D $i
+    #echo "more?"
+    #read ans
+    #if [ "$ans" = "n" ]
+    #then
+#	exit
+    #fi
+done
